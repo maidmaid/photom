bb458739d9388c78365bdbd26c7d73e54ace5397
Dany <danymaillard93b@gmail.com>
2014-06-16 00:21:31 +0200

    ajoute la page photo

--- src/Pma/ContentBundle/Controller/ContentController.php
+++ src/Pma/ContentBundle/Controller/ContentController.php
@@ -10,4 +10,30 @@ class ContentController extends Controller
     {
         return $this->render('PmaContentBundle:HomePage:index.html.twig');
     }
+    
+    public function photoPageAction()
+    {        
+        return $this->render('PmaContentBundle:PhotoPage:index.html.twig', array(
+            'services' => $this->getPhotoServices()
+        ));
+    }
+    
+    private function getPhotoServices()
+    {
+        /* @var $translator \Symfony\Component\Translation\Translator */
+        $translator = $this->get('translator');
+
+        for ($i = 1; $i <= 2; $i++)
+        {
+            $services[] = $this->renderView('PmaContentBundle:Service:service.html.twig', array(
+                'id' => $translator->trans("photo.service$i.id"),
+                'title' => $translator->trans("photo.service$i.title"),
+                'description' => $translator->trans("photo.service$i.description"),
+                'image' => $translator->trans("photo.service$i.image"),
+                'right' => $i % 2 == 0
+            ));   
+        }
+        
+        return $services;
+    }
 }
