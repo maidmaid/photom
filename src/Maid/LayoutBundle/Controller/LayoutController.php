70e66d5a86cc71e09e08b356c18f59c6dd00395f
Dany <danymaillard93b@gmail.com>
2014-06-22 22:20:59 +0200

    crée un bundle pour le layout

---
+++ src/Maid/LayoutBundle/Controller/LayoutController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Maid\LayoutBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class LayoutController extends Controller
+{
+
+}
---
+++ src/Maid/LayoutBundle/MaidLayoutBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Maid\LayoutBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MaidLayoutBundle extends Bundle
+{
+}
