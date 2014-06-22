99a94573b56efd2aa3d53f1b612f5c0771e3016a
Dany <danymaillard93b@gmail.com>
2014-06-22 14:48:36 +0200

    page video

--- src/Pma/ContentBundle/Controller/ContentController.php
+++ src/Pma/ContentBundle/Controller/ContentController.php
@@ -37,9 +37,30 @@ class ContentController extends Controller
         return $items;
     }
     
+    private function getItemsVideo()
+    {
+        /* @var $translator \Symfony\Component\Translation\Translator */
+        $translator = $this->get('translator');
+        $ids = $this->container->getParameter('videopage.items.ids');
+        $items = array();
+        
+        foreach ($ids as $i => $id)
+        {
+            $items[$i]['id'] = $translator->trans("item.video.$id.id", array(), 'videopage');
+            $items[$i]['title'] = $translator->trans("item.video.$id.title", array(), 'videopage');
+            $items[$i]['description'] = $translator->trans("item.video.$id.description", array(), 'videopage');
+            $items[$i]['image'] = $translator->trans("item.video.$id.image", array(), 'videopage');
+            $items[$i]['right'] = $i % 2 == 0;
+        }
+        
+        return $items;
+    }
+    
     public function videoPageAction()
     {
-        return $this->render('PmaContentBundle:VideoPage:index.html.twig');
+        return $this->render('PmaContentBundle:VideoPage:index.html.twig', array(
+            'items' => $this->getItemsVideo()
+        ));
     }
     
     public function internetPageAction()
