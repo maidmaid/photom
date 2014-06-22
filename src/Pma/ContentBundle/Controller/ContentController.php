694e82c9780154f2e726f194076ec0848b4c66e6
Dany <danymaillard93b@gmail.com>
2014-06-22 14:53:41 +0200

    getItemsPhoto et getItemsVideos en getItems

--- src/Pma/ContentBundle/Controller/ContentController.php
+++ src/Pma/ContentBundle/Controller/ContentController.php
@@ -14,42 +14,23 @@ class ContentController extends Controller
     public function photoPageAction()
     {        
         return $this->render('PmaContentBundle:PhotoPage:index.html.twig', array(
-            'items' => $this->getItemsPhoto()
+            'items' => $this->getItems('photopage.items.ids', 'photopage')
         ));
     }
     
-    private function getItemsPhoto()
+    private function getItems($ids, $domaine)
     {
         /* @var $translator \Symfony\Component\Translation\Translator */
         $translator = $this->get('translator');
-        $ids = $this->container->getParameter('photopage.items.ids');
+        $ids = $this->container->getParameter($ids);
         $items = array();
         
         foreach ($ids as $i => $id)
         {
-            $items[$i]['id'] = $translator->trans("item.photo.$id.id", array(), 'photopage');
-            $items[$i]['title'] = $translator->trans("item.photo.$id.title", array(), 'photopage');
-            $items[$i]['description'] = $translator->trans("item.photo.$id.description", array(), 'photopage');
-            $items[$i]['image'] = $translator->trans("item.photo.$id.image", array(), 'photopage');
-            $items[$i]['right'] = $i % 2 == 0;
-        }
-        
-        return $items;
-    }
-    
-    private function getItemsVideo()
-    {
-        /* @var $translator \Symfony\Component\Translation\Translator */
-        $translator = $this->get('translator');
-        $ids = $this->container->getParameter('videopage.items.ids');
-        $items = array();
-        
-        foreach ($ids as $i => $id)
-        {
-            $items[$i]['id'] = $translator->trans("item.video.$id.id", array(), 'videopage');
-            $items[$i]['title'] = $translator->trans("item.video.$id.title", array(), 'videopage');
-            $items[$i]['description'] = $translator->trans("item.video.$id.description", array(), 'videopage');
-            $items[$i]['image'] = $translator->trans("item.video.$id.image", array(), 'videopage');
+            $items[$i]['id'] = $translator->trans("item.video.$id.id", array(), $domaine);
+            $items[$i]['title'] = $translator->trans("item.video.$id.title", array(), $domaine);
+            $items[$i]['description'] = $translator->trans("item.video.$id.description", array(), $domaine);
+            $items[$i]['image'] = $translator->trans("item.video.$id.image", array(), $domaine);
             $items[$i]['right'] = $i % 2 == 0;
         }
         
@@ -59,7 +40,7 @@ class ContentController extends Controller
     public function videoPageAction()
     {
         return $this->render('PmaContentBundle:VideoPage:index.html.twig', array(
-            'items' => $this->getItemsVideo()
+            'items' => $this->getItems('videopage.items.ids', 'videopage')
         ));
     }
     
@@ -88,3 +69,4 @@ class ContentController extends Controller
         return $this->render('PmaContentBundle:ContactPage:index.html.twig');
     }
 }
+
